200518e9534347000abcf4faa285e86164c0a59f
Alexmv16 <71333947+Alexmv16@users.noreply.github.com>
2024-02-01 09:57:28 -0500

    Delete __manifest__.py

--- __manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Biblioteca',
-    'version': '1.0',
-    'depends': ['base'],
-    'author': 'Alex Moreno',
-    'category': 'Uncategorized',
-    'data': [],
-    'installable': True,
-}
